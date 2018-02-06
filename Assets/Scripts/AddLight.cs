91ed29ed851e89cc3ad14cd7757122b184128a3d
Flippeen <filip.k.lindgren@gmail.com>
2018-02-06 15:03:55 +0100

    Hej mitt folk

---
+++ Assets/Scripts/AddLight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddLight : MonoBehaviour {
+
+	/*void Update () {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            GetComponent<LightIntensity>().AddLightRange(2);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            GetComponent<LightIntensity>().AddLightRange(-2);
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Debug.Log(GetComponent<LightIntensity>().GetLightRange());
+        }
+    }*/
+
+    [SerializeField] float lightToAdd;
+    private void OnTriggerEnter(Collider other)
+    {
+        /*if(other.tag == "Player" && FindObjectOfType<LightIntensity>().GetComponent<LightIntensity>().GetLightRange()
+                 < FindObjectOfType<LightIntensity>().GetComponent<LightIntensity>().GetMaxLightRange() - lightToAdd)
+        {
+            FindObjectOfType<LightIntensity>().GetComponent<LightIntensity>().AddLightRange(lightToAdd);
+            Destroy(gameObject);
+        }*/
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<LightIntensity>().GetComponent<LightIntensity>().AddLightRange(lightToAdd);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/LightIntensity.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightIntensity : MonoBehaviour {
+    
+    [SerializeField] Light lightLantern;
+    float lightIntensity;
+    public float lightRange;
+    [SerializeField] float lightRemoved;
+    [SerializeField] float maxLightRange;
+    void Start()
+    {
+        /*lightIntensity = 2;
+        lightRange = 10;
+        lightRemoved = 0.5f;
+        maxLightRange = 100f;*/
+        lightRange = maxLightRange;
+    }
+    void Update()
+    {
+        if(lightRange > 0)
+        {
+            lightRange -= lightRemoved * Time.deltaTime;
+        }
+        //lt.intensity = lightIntensity;
+        lightLantern.range = lightRange;
+        if(lightRange > maxLightRange)
+        {
+            lightRange = maxLightRange;
+        }
+    }
+    public void AddLightRange(float addedLightRange)
+    {
+        if(addedLightRange > 0 && lightRange < maxLightRange)
+        {
+            Debug.Log("Lägg på ljus");
+            lightRange += addedLightRange;
+        }
+        if (addedLightRange < 0 && lightRange > 0)
+        {
+            Debug.Log("Ta bort ljus");
+            lightRange += addedLightRange;
+        }
+        else
+            Debug.Log("Redan max ljus nivå eller min ljus nivå.");
+    }
+    public float GetLightRange()
+    {
+        return lightRange;
+    }
+    public float GetMaxLightRange()
+    {
+        return maxLightRange;
+    }
+}
