deeca56cf499f2cb0ed96048c670f24e6c594c30
Anthony <a.w.colton@gmail.com>
2018-02-07 14:58:38 +0100

    ant

--- Assets/Scripts/MoveWall.cs
+++ Assets/Scripts/MoveWall.cs
@@ -6,6 +6,6 @@ public class MoveWall : MonoBehaviour {
 
     [SerializeField] float moveSpeed;
 	void Update () {
-        transform.Translate(moveSpeed, 0, 0);
+        transform.Translate(0, moveSpeed, 0);
 	}
 }
