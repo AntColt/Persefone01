8c943ed240247fd56f90c2decac7f6e2769354a2
Flippeen <filip.k.lindgren@gmail.com>
2018-02-07 11:29:57 +0100

    DeathWall

---
+++ Assets/InstatiateDeathWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstatiateDeathWall : MonoBehaviour {
+    [SerializeField] GameObject deathWall;
+    [SerializeField] GameObject spawnPoint;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && GameObject.FindGameObjectWithTag("DeathWall") == null)
+        {
+            Instantiate(deathWall, spawnPoint.transform.position, spawnPoint.transform.rotation);
+        }
+    }
+}
---
+++ Assets/MoveWall.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWall : MonoBehaviour {
+
+    [SerializeField] float moveSpeed;
+	void Update () {
+        transform.Translate(moveSpeed, 0, 0);
+	}
+}
